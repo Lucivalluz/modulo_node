40218c20ab5b2485921104c4544efdff10ac1dae
lucivalluz <lucivalsouza@hotmail.com>
2024-07-18 21:28:11 -0300

    Modulo Node

---
+++ index.js
@@ -0,0 +1,6 @@
+const {sum, multi, person} = require('./objetos')
+
+
+console.log(multi(4,5))
+console.log(sum(4,5))
+console.log(person)
---
+++ objetos.js
@@ -0,0 +1,14 @@
+function sum(firstNumber, seccondNumber) {
+    return firstNumber + seccondNumber
+}
+
+function multi(firstNumber, seccondNumber) {
+    return firstNumber * seccondNumber
+}
+
+const person = {
+    name: 'Lucival',
+    age: 43
+}
+
+module.exports = {sum, multi, person}
